f39c50b501e56f2cb8d283ec0c6f174c66780fb7
Ajmal Kurnia <ajmalprayoga@gmail.com>
2019-04-18 07:47:47 +0700

    add rouge-L performance

--- rouge_evaluation.py
+++ rouge_evaluation.py
@@ -1,23 +1,33 @@
+import numpy as np
+
 class Rouge:
     self.rouge_1_score = 0.0
     self.rouge_2_score = 0.0
-    self.rouge_l_precision = 0.0
-    self.rouge_l_recall = 0.0
-    self.rouge_l_f_score = 0.0
-    flatten = lambda l: [item for sublist in l for item in sublist]
+    self.rouge_l_score = 0.0
+    self.beta = 1.0
+    _flatten = lambda l: [item for sublist in l for item in sublist]
 
     def _lcs_length(self, seq1, seq2):
-        pass
+    n = len(seq1)
+    m = len(seq2)
+    lcs_table = np.zeros((n+1,m+1))
+    for i in range(1,n+1):
+        for j in range(1, m+1):
+            if seq1[i-1] == seq2[j-1]:
+                lcs_table[i][j] = lcs_table[i-1][j-1] + 1.0
+            else:
+                lcs_table[i][j] = max(lcs_table[i][j-1], lcs_table[i-1][j])
+    return lcs_table[n][m]
 
     def _build_bigram_vocabulary(sequences):
         bigram = []
         for sequence in a:
             bigram.append([(sequence[word], sequence[word+1]) for word in range(len(sequence)-1)])
-        return set(Rouge.flatten(bigram))
+        return set(Rouge._flatten(bigram))
 
     def rouge_1(self, reference_summary, choosen_sentence):
-        reference_summary = Rouge.flatten(reference_summary)
-        choosen_sentence = Rouge.flatten(choosen_sentence)
+        reference_summary = Rouge._flatten(reference_summary)
+        choosen_sentence = Rouge._flatten(choosen_sentence)
         reference_unigram = set(reference_summary)
         choosen_unigram = set(choosen_sentence)
         match_unigram = 0
@@ -46,10 +56,15 @@ class Rouge:
         self.rouge_2_score = 2.0/((1.0/self.rouge_2_recall)+(1.0/self.rouge_2_precision))
 
     def rouge_l(reference_summary, choosen_sentence):
-        pass
+        reference_summary = Rouge._flatten(reference_summary)
+        choosen_sentence = Rouge._flatten(choosen_sentence)
+        lcs_len = self._lcs_length(reference_summary, choosen_sentence)
+        self.rouge_l_recall = 1.0*lcs_len/len(reference_summary)
+        self.rouge_l_precision = 1.0*lcs_len/len(choosen_sentence)
+        self.rouge_l_score = ((1+self.beta**2)*self.rouge_l_recall*self.rouge_l_precision)/
+                                (self.rouge_l_recall+ ((self.beta**2)*self.rouge_l_precision))
 
     def compute_all(reference_summary, choosen_sentence):
-
         rouge_1(reference_summary, choosen_sentence)
         rouge_2(reference_summary, choosen_sentence)
         rouge_l(reference_summary, choosen_sentence)
